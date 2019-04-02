f7fe301ed4be8f9e927eb3b2303addf6f7bda49c
Rene <rene.lozano@tecsup.edu.pe>
2019-04-01 22:03:47 -0500

    Laboratorio01

---
+++ lab03/tecsupApp/App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Image>
+        source={require('./logo.png')}
+      </Image>
+      <Text style={styles.welcome}>Bienvenida Alumno Tecsup!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
